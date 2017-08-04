041feb727925e8bd714efac7bf6e3824e589cf29
DESKTOP-7C4Q5D0\ChrisWong-PC <invysti@gmail.com>
2017-08-04 17:28:08 -0500

    Added getPlayers method and established some preliminary static integers.

--- src/HoldemGame.java
+++ src/HoldemGame.java
@@ -3,9 +3,15 @@ import java.lang.Math;
 
 public class HoldemGame {
 
+	public static final int MAX_PLAYERS = 10;
+	public static final int CARDS = 52;
+	
 	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
 		
 		// TODO get number of players from user
+		int players = getPlayers(keyboard);
+		System.out.printf("There are %d players.", players);
 		
 		// TODO calculate number of cards to be dealt
 		
@@ -17,4 +23,18 @@ public class HoldemGame {
 
 	}
 
+	public static int getPlayers(Scanner keyboard) {
+		int player;
+		System.out.println("Please enter the number of players.");
+		do {
+			System.out.print("Please enter an amount between 1 and 10: ");
+			while (!keyboard.hasNextInt()) {
+				System.out.print("Please enter an integer: ");
+				keyboard.next();
+			}
+			player = keyboard.nextInt();
+		} while (player < 1 || player > 10);
+		return player;
+	}
+	
 }
