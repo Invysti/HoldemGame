f690cd06346ddf8f1fc11034f134decce83d08a9
DESKTOP-7C4Q5D0\ChrisWong-PC <invysti@gmail.com>
2017-08-04 16:24:40 -0500

    Imported basic libraries and added starting pseudocode.

--- src/HoldemGame.java
+++ src/HoldemGame.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class HoldemGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// TODO get number of players from user
+		
+		// TODO calculate number of cards to be dealt
+		
+		// TODO calculate number of cards left in deck
+		
+		// TODO consider how game should be played
+		
+		// TODO figure out card data structure
 
 	}
 
